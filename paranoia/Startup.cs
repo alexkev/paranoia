5252c895567bb05f76d57bfb87cbf897f3450fed
Tyler Russ <tylerjruss27@gmail.com>
2019-04-04 09:38:26 -0600

    Migration/DB

--- paranoia/Startup.cs
+++ paranoia/Startup.cs
@@ -23,11 +23,12 @@ namespace paranoia
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ParanoiaContext>(opt =>
-                opt.UseInMemoryDatabase("Paranoia"));
+           
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-          
+            services.AddDbContext<ParanoiaContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ParanoiaContext")));
+
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
