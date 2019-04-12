895b9084a8bc2d2e1e482651ceb01bca6a797264
Alexkev <Alex.kmatheson@gmail.com>
2019-04-12 15:12:49 -0600

    deleted unneeded pages and assets

--- paranoia/ClientApp/src/app/app.module.ts
+++ paranoia/ClientApp/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ParanoiaComponent } from './paranoia/paranoia.component';
 import { CardsComponent } from './paranoia/cards/cards.component';
 
@@ -20,8 +18,6 @@ import { CardsEditComponent } from './paranoia/cards/cards-edit/cards-edit.compo
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     ParanoiaComponent,
     CardsComponent,
     CardsEditComponent
@@ -32,8 +28,6 @@ import { CardsEditComponent } from './paranoia/cards/cards-edit/cards-edit.compo
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
       { path: 'paranoia', component: ParanoiaComponent, children: [
           { path: 'edit/:id', component: CardsEditComponent }
       ] }
